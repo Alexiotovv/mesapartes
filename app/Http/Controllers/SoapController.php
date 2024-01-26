99198193bdba0da4dfa1b418257d9c01cd0734ad
Alexiotovv <gpsalexvasquez@gmail.com>
2024-01-26 10:16:59 -0500

    uncomment_code

--- app/Http/Controllers/SoapController.php
+++ app/Http/Controllers/SoapController.php
@@ -9,6 +9,8 @@ use Illuminate\Http\Request;
 use nusoap_client;
 
 
+
+
 class SoapController extends Controller
 {
     public function consumeSoap(Request $request)
@@ -17,23 +19,19 @@ class SoapController extends Controller
         // try {
 
             // Configurar el servicio SOAP
+            $wsdl = 'https://ws3.pide.gob.pe/services/PcmIMgdEntidad.PcmIMgdEntidadHttpsSoap11Endpoint';
+            $client = new nusoap_client($wsdl, 'wsdl');
+            $client->soap_defencoding = 'UTF-8';
+            $client->decode_utf8 = false;
 
+            // Parámetros para la llamada al servicio
+            $params = ['sidcatent' => 1];
 
-            // $wsdl = 'https://ws3.pide.gob.pe/services/PcmIMgdEntidad.PcmIMgdEntidadHttpsSoap11Endpoint';
-            // $client = new nusoap_client($wsdl, 'wsdl');
-            // $client->soap_defencoding = 'UTF-8';
-            // $client->decode_utf8 = false;
-
-            // // Parámetros para la llamada al servicio
-            // $params = ['sidcatent' => 1];
-
-            // // Hacer la solicitud SOAP
-            // $response = $client->call('getListaEntidad', $params);
+            // Hacer la solicitud SOAP
+            $response = $client->call('getListaEntidad', $params);
     
-            // // Manejar la respuesta como desees
-            // return response()->json($response);
-
-
+            // Manejar la respuesta como desees
+            return response()->json($response);
 
         // } catch (\Throwable $e) {
         //     return response()->json(['error' => 'Error desconocido: ' . $e->getMessage()]);
