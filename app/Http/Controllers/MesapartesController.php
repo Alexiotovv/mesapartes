6ca34c9666df64ba947a08a2b19db12289abb722
Alexiotovv <gpsalexvasquez@gmail.com>
2024-03-15 13:06:17 -0500

    docu

--- app/Http/Controllers/MesapartesController.php
+++ app/Http/Controllers/MesapartesController.php
@@ -31,7 +31,7 @@ class MesapartesController extends Controller
     }
     public function test(Request $request)
     {
-        $documento = $request->file('documento');
+        $documento = request('documento');
 
         $response = Http::withoutVerifying()->post('https://aplicaciones04.regionloreto.gob.pe/mpv/file_controller.php', [
             'user' => 'mesapartevirutal199.',
